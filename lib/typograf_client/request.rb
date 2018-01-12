17cd95e354fe7741b17670928831e9dd9b65f80f
Yury Izotov <bestlowry@gmail.com>
2018-01-12 06:29:22 +0500

    all style fix

--- lib/typograf_client/request.rb
+++ lib/typograf_client/request.rb
@@ -1,23 +1,24 @@
 require 'savon'
 
 module TypografClient
+  # main class for Request to service
   class Request
     attr_reader :text
 
     def initialize(text)
-      raise ArgumentError, 'text argument to typography must be given' if !text.is_a? String || text.empty?
+      raise ArgumentError unless (text.is_a? String) || !text.empty?
       @text = text
       @client = Savon.client(wsdl: 'http://typograf.artlebedev.ru/webservices/typograf.asmx?WSDL')
       @result = nil
     end
 
     def result
-      @result ||= get_response.to_s # not object - pure String returns
+      @result ||= response.to_s # not object - pure String returns
     end
 
     private
 
-    def get_response
+    def response
       response = @client.call(:process_text, message: { text: text })
       response.body[:process_text_response][:process_text_result]
     end
